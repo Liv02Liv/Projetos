005a10a175e4d1a31cdbde528b200d50d712c0f5
Livia Rocha <liviarocha640@gmail.com>
2026-06-13 10:42:13 -0300

    Calculadora

---
+++ Calculadora.py
@@ -0,0 +1,18 @@
+num1 = int(input("Digite um Numero: "))
+num2 = int(input("Digite outro Numero: "))
+
+oper = input("Escolha um Operador; (+), (-), (*), (/): ")
+
+if oper == "+":
+    print(num1 + num2)
+
+elif oper == "-":
+    print(num1 - num2)
+
+elif oper == "*":
+    print(num1 * num2)
+
+else:
+    print(num1 / num2)
+
+    
