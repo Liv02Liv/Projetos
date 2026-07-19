edc6156aae8437b8090abfbaefa8a27e9d1067c1
Livia Rocha <liviarocha640@gmail.com>
2026-07-19 04:48:44 -0300

    Ataque e Defesa

--- Copa do Mundo da FIFA 2026™/Fase-Grupo.py
+++ Copa do Mundo da FIFA 2026™/Fase-Grupo.py
@@ -33,3 +33,28 @@ df.nlargest(10, "VIT")[["Equipes", "VIT"]].style.hide(axis="index")
 
 print("Quem perdeu mais partidas?")
 df.nlargest(10, "DER")[["Equipes", "DER"]].style.hide(axis="index")
+
+#%%
+
+print("⚽ Ataque e 🛡️ Defesa")
+
+#%%
+
+print("Melhor ataque")
+df.nlargest(10, "GM")[["Equipes", "GM"]].style.hide(axis="index")
+
+#%%
+
+print("Melhor defesa")
+df.nsmallest(10, "GC")[["Equipes", "GC"]].style.hide(axis="index")
+
+#%%
+
+print("Existe relação entre gols marcados e pontos?")
+df[["GM", "Pts"]].corr().style.hide(axis="index")
+#Se o valor estiver próximo de 1, existe uma forte relação positiva.
+
+#%%
+
+print("Melhor saldo de gols")
+df.nlargest(10, "SG")[["Equipes", "SG"]].style.hide(axis="index")
