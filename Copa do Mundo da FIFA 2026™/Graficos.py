44b44cde8a57b2c6906589525bf8e613d55ee154
Livia Rocha <liviarocha640@gmail.com>
2026-07-21 09:53:08 -0300

    Pontos por equipe

---
+++ Copa do Mundo da FIFA 2026™/Graficos.py
@@ -0,0 +1,21 @@
+#%%
+
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+df = pd.read_csv("Copa-do-Mundo-da-FIFA-2026.csv", index_col= False, sep= ";")
+df
+
+#%%
+
+print("Pontos por equipe")
+df.sort_values("Pts", ascending=False).plot(
+    x="Equipes",
+    y="Pts",
+    kind="bar",
+    figsize=(12,5)
+)
+
+plt.show()
+
+#%%
