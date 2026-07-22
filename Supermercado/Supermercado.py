cc05b204f4231c288f18a3f73b2dca130b003fe0
Livia Rocha <liviarocha640@gmail.com>
2026-07-22 10:48:18 -0300

    Produtos

--- Supermercado/Supermercado.py
+++ Supermercado/Supermercado.py
@@ -1,4 +1,28 @@
 #%%
-print("=" * 50)
+import pandas as pd
+
+print("=" * 60)
 print("SUPERMERCADO - CAIXA 01".center(50))
-print("=" * 50)
+print("=" * 60)
+print("Digite o codigo do produto e pressione a tecla Entre.")
+print("Digite 'F' para finalizar a compra.")
+
+print("-" * 60)
+
+dados = {
+    "Código": ["001", "002", "003", "004", "005", "006", "007", "008"],
+    "Produto": [
+        "Arroz 5kg",
+        "Feijão 1kg",
+        "Macarrão 500g",
+        "Óleo de Soja 500ml",
+        "Açúcar 1kg",
+        "Café 500g",
+        "Leite 1l",
+        "Sabonete"],
+    "Preço": [24.90, 8.50, 4.30, 7.28, 5.10, 15.98, 4.80, 2.50]
+}
+
+df = pd.DataFrame(dados).set_index("Código")
+
+print(df)
